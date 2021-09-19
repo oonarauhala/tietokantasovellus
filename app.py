58fd49fe26d0d5f133250d5949568c76fdcded8b
Oona <oona.e.rauhala@gmail.com>
2021-09-19 18:56:28 +0300

    Implement basic connection to db

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    sql = "SELECT * FROM areas;"
+    result = db.session.execute(sql)
+    area = result.fetchone()
+    try: 
+        return f"Area selected: {area[1]}"
+    except:
+        return render_template("index.html")
 
 @app.route("/admin")
 def admin():
