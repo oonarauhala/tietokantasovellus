ffca3f152e46f9e6e02f1162cffddcb9dd6e82aa
Oona <oona.e.rauhala@gmail.com>
2021-10-05 15:26:11 +0300

    Add admin time deleting radios

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,17 +1,12 @@
 'use strict';
 
-
 const button_1_click = () => {
     document.getElementById("area_0").style.backgroundColor="#FF0000";
 };
 
-const testi = () => {
-    console.log("JS ajetaan")
-}
-
-const show_radio_in_update = () => {
+const show_radio_in_update = (class_name, select_id) => {
     // Hide all radios
-    var radios = document.getElementsByClassName("update_radio")
+    var radios = document.getElementsByClassName(class_name)
     for (let i=0; i<radios.length; i++){
         let item = radios[i]
         if (item.hasAttribute("hidden") != true){
@@ -19,16 +14,14 @@ const show_radio_in_update = () => {
         };
     };
     // Get select value(dinosaur id)
-    var select = document.getElementById('select');
+    var select = document.getElementById(select_id);
     var dinosaur_id = select.options[select.selectedIndex].value;
-    console.log("Selected dino id: ")
-    console.log(dinosaur_id)
-    // Get all elements by name dinosaur_id
-    var elements = document.getElementsByName(dinosaur_id);
+    // Get all elements with name dinosaur_id AND class class_name
+    var elements_name = Array.from(document.getElementsByName(dinosaur_id));
+    var elements_class = Array.from(document.getElementsByClassName(class_name));
+    var elements = elements_name.filter(x => elements_class.includes(x));
     for (let i=0; i<elements.length; i++){
         let item = elements[i]
-        console.log("Emenent to be shown: ")
-        console.log(item)
         if (item.name = dinosaur_id) {
             item.removeAttribute("hidden")
         };
